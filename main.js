b341df5b6909fd2741f105642806e16edccbcdad
Laila ofzo <mynameislaila@outlook.com>
2022-10-13 21:08:15 +0200

    opzet class

--- main.js
+++ main.js
@@ -4,21 +4,35 @@ window.addEventListener('load', _=>{
         constructor(){
             //data
             this.gamebox = document.getElementById('memory-game');
+            this.label = 'img/blik.png';
         }
         //methods
     loadCardsToPage()  {
         for (let i = 0; i < 16; i++){
-            this.gamebox.innerHTML += '<img src="img/eyes.png" class="card">';
+            this.gamebox.innerHTML += `<img src="img/eyes.png" class="card" value="${i}">`;
         }
     }
-    flipCard() {
-        console.log('card flipped');
+    flipCard(){
+        this.gamebox.addEventListener('click', (e)=>{
+            //checked of er op een image is gedrukt
+            if(e.target.classList.contains('card')){
+                e.target.src = `${this.label}`
+            }
+        })
+    }
+    overrideLabel (){
+        this.label = 'img/cat.png';
+    }
+    //de game kan ik hier programmeren!
+    memoryGame() {
+        this.loadCardsToPage();
+        this.flipCard();
     }
     }
     
     
     
     let memory = new Memory();
-    memory.loadCardsToPage();
-
+    memory.memoryGame();
+    //memory.overrideLabel();
 })
