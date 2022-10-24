c7702e4ae7ec8bc45742ab7780884bc4b2270adc
laila <91194082+Lailaismyname@users.github.com>
2022-10-24 16:33:52 +0200

    rommel verwijderd

--- main.js
+++ main.js
@@ -18,6 +18,17 @@ window.addEventListener('load', _=>{
             this.gamebox.innerHTML += `<img src="img/eyes.png" class="card"  id="card${i}" value="${i}">`;
         }
     }
+    newGame(){
+         //reset all counters
+         this.guessed = 0;
+         this.clicks = 0;
+         //empty the gamebox
+         this.clearGameBox();
+         //shuffle cards
+         this.shuffleImages();
+         //create a new game
+         this.loadCardsToBox();
+    }
     shuffleImages(){
         this.images.sort(function () {
                 return Math.random() - 0.5;
@@ -35,6 +46,46 @@ window.addEventListener('load', _=>{
     clearGameBox(){
         this.gamebox.innerHTML = '';
     }
+    compareCards(e){
+        //save value of card
+        this.currentCard = e.target.getAttribute('value');
+        //save card id
+        this.currentGuessId =  e.target.id;
+        //add checked class, checked is to keep track of which card has been clicked on. 
+        e.target.classList.add('checked');
+        //Asign img to cards. 
+        this.setCardImgSrc();
+        e.target.src = `${this.label}`;
+        //update clickcounter
+        this.clicks++;
+        //if 2 cards have been clicked check if they are equal
+        if(this.clicks == 2){
+            //if cards are equal:
+            if(e.target.src == this.prevGuess){
+                //make card unclickable
+                e.target.style.pointerEvents = 'none';
+                //add guessed class to mark card ass guessed.
+                e.target.classList.add('guessed');
+                //make previous card unclickable, and mark previous card as guessed
+                document.getElementById(this.prevCardId).style.pointerEvents = 'none';
+                document.getElementById(this.prevCardId).classList.add('guessed');
+                //update guessed card counter. 
+                this.guessed++;
+            }
+            //if cards are not equal
+            else{
+                //flip cards back after a second
+                setTimeout(() => {
+                    this.flipCardsBack();
+                }, 1000);
+            }
+            //reset clickcounter
+            this.clicks = 0;
+        }
+        //save clicked cards for future comparison
+        this.prevGuess = e.target.src;
+        this.prevCardId = e.target.id;
+    }
     flipCardsBack(){
         this.checked = document.querySelectorAll('.checked');
         this.checked.forEach(item => {
@@ -49,41 +100,13 @@ window.addEventListener('load', _=>{
     flipCard(){
         this.gamebox.addEventListener('click', (e)=>{
             if(e.target.classList.contains('card')){
-                //sla waarde van kaart op in var
-                this.currentCard = e.target.getAttribute('value');
-                //sla id op in var
-                this.currentGuessId =  e.target.id;
-                //voeg checked class toe. checked is om bij te houden op welke er geklikt is. 
-                e.target.classList.add('checked');
-                //wijs de afbeelding toe aan kaarten
-                this.setCardImgSrc();
-                e.target.src = `${this.label}`;
-                this.clicks++;
-                if(this.clicks == 2){
-                    if(e.target.src == this.prevGuess){
-                        e.target.style.pointerEvents = 'none';
-                        //guessed class is om het totaal aantal geraden kaarten aan te duiden.
-                        e.target.classList.add('guessed');
-                        document.getElementById(this.prevCardId).style.pointerEvents = 'none';
-                        document.getElementById(this.prevCardId).classList.add('guessed');
-                        this.guessed++;
-                    }
-                    else{
-                        setTimeout(() => {
-                            this.flipCardsBack();
-                        }, 1000);
-                    }
-                    this.clicks = 0;
-                }
-                this.prevGuess = e.target.src;
-                this.prevCardId = e.target.id;
+                this.compareCards(e);
             }
+            //if all pairs have been found, a button with new game appears, if that is clicked:
             else if(e.target.classList.contains('new-game')){
-                this.guessed = 0;
-                this.clicks = 0;
-                this.clearGameBox();
-                this.loadCardsToBox();
+               this.newGame();
             }
+            //if all pairs have been found, print the winscreen
             if(this.guessed == 8){
                 this.printWinScreen();
                 }
@@ -103,4 +126,4 @@ window.addEventListener('load', _=>{
 })
 
 //enige probleem is door die settimeout kan het zo zijn dat er te snel op een nieuwe knop word gedrukt en dan loopt het allemaal scheef.
-//misschien kan ik een manier vinden om events pas te laten afspele nadat die settimeout met flipcardsback is voltooid?
+//callback --> promises --> async 
